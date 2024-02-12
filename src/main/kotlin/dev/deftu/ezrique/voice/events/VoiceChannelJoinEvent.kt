3516c4eba80d5f9e08d7adb2bb37b95d015a7307
Deftu <deftudev@gmail.com>
2024-02-12 15:28:48 +0200

    Update VoiceChannelJoinEvent to use Kord events

--- src/main/kotlin/dev/deftu/ezrique/voice/events/VoiceChannelJoinEvent.kt
+++ src/main/kotlin/dev/deftu/ezrique/voice/events/VoiceChannelJoinEvent.kt
@@ -1,8 +1,14 @@
 package dev.deftu.ezrique.voice.events
 
+import dev.kord.common.annotation.KordPreview
 import dev.kord.common.entity.Snowflake
+import dev.kord.core.Kord
+import dev.kord.core.event.Event
 
 data class VoiceChannelJoinEvent(
+    override val kord: Kord,
+    @KordPreview override val customContext: Any?,
+    override val shard: Int,
     val guildId: Snowflake,
-    val channelId: Snowflake
-)
+    val channelId: Snowflake,
+) : Event
