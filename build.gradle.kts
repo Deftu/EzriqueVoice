b90512def532876d12eca909edb02c3091e935ff
Deftu <deftudev@gmail.com>
2024-02-12 15:28:28 +0200

    Use ezrique-core for shaded libraries

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,31 +11,35 @@ plugins {
 }
 
 repositories {
+    maven("https://maven.deftu.dev/internal") {
+        fun getPublishingUsername(): String? {
+            val property = project.findProperty("deftu.publishing.username")
+            return property?.toString() ?: System.getenv("DEFTU_PUBLISHING_USERNAME")
+        }
+
+        fun getPublishingPassword(): String? {
+            val property = project.findProperty("deftu.publishing.password")
+            return property?.toString() ?: System.getenv("DEFTU_PUBLISHING_PASSWORD")
+        }
+
+        val publishingUsername = getPublishingUsername()
+        val publishingPassword = getPublishingPassword()
+        if (publishingUsername != null && publishingPassword != null) {
+            credentials {
+                username = publishingUsername
+                password = publishingPassword
+            }
+        }
+    }
+
     maven("https://m2.dv8tion.net/releases")
 }
 
 dependencies {
-    // Discord
-    shade(implementation("dev.kord:kord-core:${libs.versions.kord.get()}")!!)
+    shade(implementation("dev.deftu:ezrique-core:${libs.versions.core.get()}")!!)
     shade(implementation("dev.kord:kord-core-voice:${libs.versions.kord.get()}")!!)
     shade(implementation("dev.kord:kord-voice:${libs.versions.kord.get()}")!!)
-
-    // Data handling
     shade(implementation("com.sedmelluq:lavaplayer:${libs.versions.lavaplayer.get()}")!!)
-    shade(implementation("com.google.code.gson:gson:${libs.versions.gson.get()}")!!)
-    shade(implementation("xyz.deftu:enhancedeventbus:${libs.versions.enhancedeventbus.get()}")!!)
-    shade(implementation("com.squareup.okhttp3:okhttp:${libs.versions.okhttp.get()}")!!)
-
-    // SQL
-    shade(implementation("org.xerial:sqlite-jdbc:${libs.versions.sqlitejdbc.get()}")!!)
-    shade(implementation("org.jetbrains.exposed:exposed-core:${libs.versions.exposed.get()}")!!)
-    shade(implementation("org.jetbrains.exposed:exposed-dao:${libs.versions.exposed.get()}")!!)
-    shade(implementation("org.jetbrains.exposed:exposed-jdbc:${libs.versions.exposed.get()}")!!)
-
-    // Logging
-    shade(implementation("org.apache.logging.log4j:log4j-api:${libs.versions.log4j.get()}")!!)
-    shade(implementation("org.apache.logging.log4j:log4j-core:${libs.versions.log4j.get()}")!!)
-    shade(implementation("org.apache.logging.log4j:log4j-slf4j-impl:${libs.versions.log4j.get()}")!!)
 }
 
 tasks {
